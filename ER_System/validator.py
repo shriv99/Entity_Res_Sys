3367ba57284856a6fc2830a2781993d50dbf5191
shriv99 <shriv99@gmail.com>
2022-08-26 10:11:48 -0400

    Update validator.py

--- ER_System/validator.py
+++ ER_System/validator.py
@@ -6,6 +6,8 @@ df = pd.read_json(val, lines=True)
 df_matches = df[df.match==1]
 df_matches = df_matches.rename({'left': 'source_a', 'right': 'source_b'}, axis=1)
 df_matches['partial_ratio'] = df_matches.apply(lambda x: fuzz.partial_ratio(x['source_a'], x['source_b']), axis=1 )
+df_matches['token_set_ratio'] = df_matches.apply(lambda x: fuzz.token_set_ratio(x['source_a'], x['source_b']), axis=1 )
+df_matches['W_ratio'] = df_matches.apply(lambda x: fuzz.WRatio(x['source_a'], x['source_b']), axis=1 )
 
 df_matches.to_excel("Validator_ratio.xlsx")  
 
