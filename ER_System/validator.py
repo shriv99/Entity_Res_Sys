fcf7759fd45d3af3904259a47fa889f880bc6705
shriv99 <shriv99@gmail.com>
2022-08-26 10:02:04 -0400

    Create validator.py

---
+++ ER_System/validator.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from fuzzywuzzy import process, fuzz
+val = input("Enter output result folder path: ")
+print(val)
+df = pd.read_json('val', lines=True)
+df_matches = df[df.match==1]
+df_matches = df_matches.rename({'left': 'source_a', 'right': 'source_b'}, axis=1)
+df_matches['partial_ratio'] = df_matches.apply(lambda x: fuzz.partial_ratio(x['source_a'], x['source_b']), axis=1 )
+
+df_matches.to_excel("Validator_ratio.xlsx")  
+
